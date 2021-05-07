28e96f2382ca4f378bfda4dba1b3e8e8c9a01e1d
Macbook Pro <itsmeadedayoe@gmail.com>
2021-05-08 00:33:36 +0100

    entern

--- task-app/config/filesystems.php
+++ task-app/config/filesystems.php
@@ -8,14 +8,14 @@ return [
     |--------------------------------------------------------------------------
     |
     | Here you may specify the default filesystem disk that should be used
-    | by the framework. The "local" disk, as well as a variety of cloud
+    | by the framework. The "local" disk, as well as a variety of cloude
     | based disks are available to your application. Just store away!
     |
     */
 
     'default' => env('FILESYSTEM_DRIVER', 'local'),
 
-    /*
+    /* believe or not 
     |--------------------------------------------------------------------------
     | Default Cloud Filesystem Disk
     |--------------------------------------------------------------------------
